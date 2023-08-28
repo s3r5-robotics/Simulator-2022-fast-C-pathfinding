d79bfd008f19f6160ffec9242a720dea117f17a0
tevzi2 <tevz.beskovnik@gmail.com>
2023-08-28 13:35:28 +0200

    refactor

--- main.c
+++ main.c
@@ -1,49 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-#include "./heap.h"
+#include "./pathfinding.h"
 
 #define X_LEN 360 // 96302, 96571 rez = 269
 #define Y_LEN 270
-
-#define SQRT2 1.414213562373095
-
-#define true (1)
-#define false (0)
-
-#define WEIGHT 1
-
-#define MANHATTAN(A, B) A + B
-
-#define EUCLIDIAN(A, B) sqrt(A *A + B * B)
-
-#define DIAGONAL_MOVEMENT true
-
-typedef unsigned char bool;
-
-typedef struct
-{
-    int minX;
-    int minY;
-
-    int maxX;
-    int maxY;
-} Area;
-
-typedef struct
-{
-    int X, Y;
-} Point;
-
-typedef struct
-{
-    int x, y;
-    double g, f, h;
-    bool walkable : 1;
-    bool closed : 1;
-    bool opened : 1;
-} Tile;
-
-Tile map[Y_LEN][X_LEN];
 
 Area wall1GT = {89, 39, 261, 71};
 Area wall2GT = {232, 39, 261, 135};
@@ -55,29 +15,26 @@ Area wall7GT = {156, 124, 203, 157};
 
 Point startPoint = {301, 131};
 Point endPoint = {61, 244};
-Point *path = NULL;
-
-Node_heap_s heap;
 
 // seed maps
-void seed_maps()
+void seed_maps(Map map)
 {
-    for (int i = 0; i < Y_LEN; i++)
+    for (int i = 0; i < map.height; i++)
     {
-        for (int j = 0; j < X_LEN; j++)
+        for (int j = 0; j < map.width; j++)
         {
-            map[i][j].x = j;
-            map[i][j].y = i;
-            map[i][j].walkable = true;
-            map[i][j].closed = false;
-            map[i][j].opened = false;
-            map[i][j].h = -1;
+            map.map[i][j].x = j;
+            map.map[i][j].y = i;
+            map.map[i][j].walkable = true;
+            map.map[i][j].closed = false;
+            map.map[i][j].opened = false;
+            map.map[i][j].h = -1;
         }
     }
 }
 
 // register zones
-void register_zone(Area area)
+void register_zone(Tile** map, Area area)
 {
     /*
         typedef struct {
@@ -98,16 +55,16 @@ void register_zone(Area area)
 }
 
 // initialize the world
-void init_world()
+void init_world(Map map)
 {
-    seed_maps();
-    register_zone(wall1GT);
-    register_zone(wall2GT);
-    register_zone(wall3GT);
-    register_zone(wall4GT);
-    register_zone(wall5GT);
-    register_zone(wall6GT);
-    register_zone(wall7GT);
+    seed_maps(map);
+    register_zone(map.map, wall1GT);
+    register_zone(map.map, wall2GT);
+    register_zone(map.map, wall3GT);
+    register_zone(map.map, wall4GT);
+    register_zone(map.map, wall5GT);
+    register_zone(map.map, wall6GT);
+    register_zone(map.map, wall7GT);
 }
 
 float slope(Point p1, Point p2)
@@ -117,29 +74,16 @@ float slope(Point p1, Point p2)
     return (float)(fabs((float)p2.Y - (float)p1.Y) / fabs((float)p2.X - (float)p1.X));
 }
 
-bool castRay(Point p1, Point p2)
-{
-    float funcSlope = slope(p1, p2);
-    int w = abs(p1.X - p2.X), h = abs(p1.Y - p2.Y);
-    int stepLenght = w > h ? round((float)w / (float)h) : round((float)h / (float)w);
-    if (stepLenght > (w > h ? w : h))
-        stepLenght = 0;
-    int smallerIncrementer = 0;
-
-    for (int i = 0; i <= (w > h ? w : h); i++)
-    {
-        smallerIncrementer += (i % stepLenght == 0 && stepLenght != 0);
-        int y = ((w > h) * smallerIncrementer + (!(w > h)) * i), x = ((w > h) * i + (!(w > h)) * smallerIncrementer),
-            smallerX = p1.X < p2.X ? p1.X : p2.X, smallerY = p1.Y < p2.Y ? p1.Y : p2.Y;
-        // printf("T(%d, %d), Incrementer: %d, X: %d, Y: %d, Step length: %d\n", smallerX + x, smallerY + y, smallerIncrementer, x, y, stepLenght);
-        if (map[smallerY + y][smallerX + x].walkable == false)
-            return false;
-    }
-
-    return true;
-}
-
-int rayPointReducer(unsigned int size)
+/**
+ * @brief reduces number of points only to only key points (does not work perfectly)
+ * 
+ * @param path path gotten from pathfinding function
+ * 
+ * @param size leght of array
+ * 
+ * @returns Path struct with reduced path
+*/
+Path ray_point_reducer(Point* path, unsigned int size)
 {
     Point point1, point2;
     point1.X = path[0].X;
@@ -190,225 +134,38 @@ int rayPointReducer(unsigned int size)
 
     printf("Len: %d\n", outArraySize);
     // printf("outArraySize: %d\n", outArraySize);
-    return outArraySize;
-}
-
-int getNeighbours(Tile map[Y_LEN][X_LEN], Node_s node, Node_s *out)
-{
-    int count = 0;
-    int px = node.x + 1, mx = node.x - 1, py = node.y + 1, my = node.y - 1;
-
-    if (!(my < 0 || mx < 0) && map[my][mx].walkable == true) // x, y, 0, 0, 0
-    {
-        out[count].parent = NULL;
-        out[count].f = map[my][mx].f;
-        out[count].h = map[my][mx].h;
-        out[count].g = map[my][mx].g;
-        out[count].y = my;
-        out[count++].x = mx;
-    }
-    if (!(Y_LEN < py || mx < 0) && map[py][mx].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[py][mx].f;
-        out[count].h = map[py][mx].h;
-        out[count].g = map[py][mx].g;
-        out[count].y = py;
-        out[count++].x = mx;
-    }
-    if (!(my < 0 || X_LEN < px) && map[my][px].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[my][px].f;
-        out[count].h = map[my][px].h;
-        out[count].g = map[my][px].g;
-        out[count].y = my;
-        out[count++].x = px;
-    }
-    if (!(Y_LEN < py || X_LEN < px) && map[py][px].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[py][px].f;
-        out[count].h = map[py][px].h;
-        out[count].g = map[py][px].g;
-        out[count].y = py;
-        out[count++].x = px;
-    }
-    if (!(X_LEN < px) && map[node.y][px].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[node.y][px].f;
-        out[count].h = map[node.y][px].h;
-        out[count].g = map[node.y][px].g;
-        out[count].y = node.y;
-        out[count++].x = px;
-    }
-    if (!(my < 0) && map[my][node.x].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[my][node.x].f;
-        out[count].h = map[my][node.x].h;
-        out[count].g = map[my][node.x].g;
-        out[count].y = my;
-        out[count++].x = node.x;
-    }
-    if (!(mx < 0) && map[node.y][mx].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[node.y][mx].f;
-        out[count].h = map[node.y][mx].h;
-        out[count].g = map[node.y][mx].g;
-        out[count].y = node.y;
-        out[count++].x = mx;
-    }
-    if (!(Y_LEN < py) && map[py][node.x].walkable == true)
-    {
-        out[count].parent = NULL;
-        out[count].f = map[py][node.x].f;
-        out[count].h = map[py][node.x].h;
-        out[count].g = map[py][node.x].g;
-        out[count].y = py;
-        out[count++].x = node.x;
-    }
-    return count;
-}
-
-int backtrace(Node_s *endNode)
-{
-    int len = 1;
-    Point *endPath = (Point *)malloc(len * sizeof(Point));
-    Node_s *current = endNode;
-    endPath[len - 1].X = current->x;
-    endPath[len - 1].Y = current->y;
-    while (current->parent != NULL)
-    {
-        len++;
-        current = current->parent;
-        endPath = (Point *)realloc(endPath, sizeof(Point) * len);
-        endPath[len - 1].X = current->x;
-        endPath[len - 1].Y = current->y;
-    }
-
-    path = (Point *)malloc(len * sizeof(Point));
-    for (int i = 0; i < len; i++)
-    {
-        path[i].X = endPath[len - 1 - i].X;
-        path[i].Y = endPath[len - 1 - i].Y;
-    }
-    // it gets returned in the reverse order
-    return len;
-}
-
-int path_find(Point start, Point end)
-{
-    heap.first = NULL;
-    heap.last = NULL;
-    Node_s *startNode = (Node_s *)malloc(sizeof(Node_s));
-    Node_s *node;
-    Node_s endNode;
-    int x, y, i, l;
-    double ng;
-    endNode.x = end.X;
-    endNode.y = end.Y;
-    startNode->x = start.X;
-    startNode->y = start.Y;
-
-    startNode->f = 0.0f;
-    map[startNode->y][startNode->x].g = 0.0f;
-
-    startNode->parent = NULL;
-
-    push(&heap, startNode);
-
-    while (!is_empty(&heap))
-    {
-        node = pop(&heap);
-        if (node == NULL)
-        {
-            return -1;
-        }
-
-        // printf("P(%d, %d), F: %f\n", node->x, node->y, node->f);
-
-        map[node->y][node->x].closed = true;
-
-        if (equal_nodes(*node, endNode))
-        {
-            return backtrace(node);
-        }
-
-        Node_s neighbours[8];
-        int numNodes = getNeighbours(map, *node, neighbours);
-        // printf("NL: %d\n", numNodes);
-        for (i = 0; i < numNodes; i++)
-        {
-            Node_s *neighbour = (Node_s *)malloc(sizeof(Node_s));
-            neighbour->x = neighbours[i].x;
-            neighbour->y = neighbours[i].y;
-            neighbour->f = neighbours[i].f;
-            neighbour->g = neighbours[i].g;
-            neighbour->h = neighbours[i].h;
-            neighbour->parent = neighbours[i].parent;
-
-            x = neighbour->x;
-            y = neighbour->y;
-
-            if (map[neighbour->y][neighbour->x].closed == true)
-            {
-                // printf("NC\n");
-                free(neighbour);
-                continue;
-            }
-
-            ng = neighbour->g + ((x - node->x == 0 || y - node->y == 0) ? 1 : SQRT2);
-
-            // printf("B: %d\n", (!map[y][x].opened || ng < neighbour->g));
-            if ((!map[y][x].opened) || ng < neighbour->g)
-            {
-                neighbour->g = ng;
-                neighbour->h = neighbour->h == -1 ? WEIGHT * MANHATTAN(abs(x - endNode.x), abs(y - endNode.y)) : neighbour->h;
-                neighbour->f = neighbour->g + neighbour->h;
-                neighbour->parent = node;
-
-                map[y][x].f = neighbour->f;
-                map[y][x].g = neighbour->g;
-                map[y][x].h = neighbour->h;
-
-                if (!map[y][x].opened)
-                {
-                    // printf("P\n");
-                    map[y][x].opened = true;
-                    push(&heap, neighbour);
-                    continue;
-                }
-                else
-                {
-                    // printf("U\n");
-                    update(&heap, neighbour);
-                    continue;
-                }
-                free(neighbour);
-            }
-        }
-    }
-
-    return -1;
+    Path new_path = {
+        .length = outArraySize,
+        .path = path
+    };
+    
+    return new_path;
 }
 
 int main()
 {
-    init_world();
+    Tile** map = (Tile**)malloc(Y_LEN * sizeof(Tile*));
+
+    for(int i = 0; i < Y_LEN; i++)
+        map[i] = (Tile*)malloc(X_LEN * sizeof(Tile));
+
+    Map wrapped_map = {
+        .width = X_LEN,
+        .height = Y_LEN,
+        .map = map
+    };
+
+    init_world(wrapped_map);
     // Point start = {20, 20};
     // Point end = {60, 90};
 
-    int len = path_find(startPoint, endPoint);
+    Path path = path_find(wrapped_map, startPoint, endPoint);
 
-    printf("Lenght of path is: %d\n", len);
+    printf("Lenght of path is: %d\n", path.length);
 
-    if (len == -1)
+    if (path.length == -1)
     {
         printf("Failed to find path.\n");
-        free_heap(&heap);
         return 0;
     }
     else
@@ -416,13 +173,11 @@ int main()
         printf("Path found:\n");
     }
 
-    for (int i = 0; i < len; i++)
-        printf("T(%d, %d)\n", path[i].X, path[i].Y);
+    for (int i = 0; i < path.length; i++)
+        printf("T(%d, %d)\n", path.path[i].X, path.path[i].Y);
 
-    // len = rayPointReducer(len);
-
-    for (int i = 0; i < len; i++)
-        printf("T(%d, %d)\n", path[i].X, path[i].Y);
+    for (int i = 0; i < path.length; i++)
+        printf("T(%d, %d)\n", path.path[i].X, path.path[i].Y);
 
     for (int i = 0; i < Y_LEN; i++)
     {
@@ -430,24 +185,23 @@ int main()
         {
             if (map[i][j].walkable != true)
             {
-                printf("EE");
+                printf("E");
             }
             else
             {
                 bool b = false;
-                for (int k = 0; k < len; k++)
-                    if (path[k].X == j && path[k].Y == i)
+                for (int k = 0; k < path.length; k++)
+                    if (path.path[k].X == j && path.path[k].Y == i)
                         b = true;
 
                 if (b)
-                    printf("XX");
+                    printf("X");
                 else
-                    printf("  ");
+                    printf(" ");
             }
         }
         printf("\n");
     }
-    // maybe have multiple heaps and just free them at the end for efficiency
-    free_heap(&heap);
+
     return 0;
 }
---
+++ pathfinding.h
@@ -0,0 +1,290 @@
+#ifndef _PATHFINDING_
+#define _PATHFINDING_
+
+#include <stdio.h>
+#include <math.h>
+#include "./heap.h"
+
+#define SQRT2 1.414213562373095
+
+#define true (1)
+#define false (0)
+
+#define WEIGHT 1
+
+#define MANHATTAN(A, B) A + B
+
+#define EUCLIDIAN(A, B) sqrt(A * A + B * B)
+
+#define DIAGONAL_MOVEMENT true
+
+typedef unsigned char bool;
+
+typedef struct
+{
+    int minX;
+    int minY;
+
+    int maxX;
+    int maxY;
+} Area;
+
+typedef struct
+{
+    int X, Y;
+} Point;
+
+typedef struct
+{
+    int x, y;
+    double g, f, h;
+    bool walkable : 1;
+    bool closed : 1;
+    bool opened : 1;
+} Tile;
+
+typedef struct 
+{
+    int length;
+    Point* path;
+} Path;
+
+typedef struct
+{
+    unsigned int width;
+    unsigned int height;
+    Tile** map;
+} Map;
+
+int get_neighbours(Map map, Node_s node, Node_s *out)
+{
+    int count = 0;
+    int px = node.x + 1, mx = node.x - 1, py = node.y + 1, my = node.y - 1;
+
+    if (!(my < 0 || mx < 0) && map.map[my][mx].walkable == true) // x, y, 0, 0, 0
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[my][mx].f;
+        out[count].h = map.map[my][mx].h;
+        out[count].g = map.map[my][mx].g;
+        out[count].y = my;
+        out[count++].x = mx;
+    }
+    if (!(map.height < py || mx < 0) && map.map[py][mx].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[py][mx].f;
+        out[count].h = map.map[py][mx].h;
+        out[count].g = map.map[py][mx].g;
+        out[count].y = py;
+        out[count++].x = mx;
+    }
+    if (!(my < 0 || map.width < px) && map.map[my][px].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[my][px].f;
+        out[count].h = map.map[my][px].h;
+        out[count].g = map.map[my][px].g;
+        out[count].y = my;
+        out[count++].x = px;
+    }
+    if (!(map.height < py || map.width < px) && map.map[py][px].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[py][px].f;
+        out[count].h = map.map[py][px].h;
+        out[count].g = map.map[py][px].g;
+        out[count].y = py;
+        out[count++].x = px;
+    }
+    if (!(map.width < px) && map.map[node.y][px].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[node.y][px].f;
+        out[count].h = map.map[node.y][px].h;
+        out[count].g = map.map[node.y][px].g;
+        out[count].y = node.y;
+        out[count++].x = px;
+    }
+    if (!(my < 0) && map.map[my][node.x].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[my][node.x].f;
+        out[count].h = map.map[my][node.x].h;
+        out[count].g = map.map[my][node.x].g;
+        out[count].y = my;
+        out[count++].x = node.x;
+    }
+    if (!(mx < 0) && map.map[node.y][mx].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[node.y][mx].f;
+        out[count].h = map.map[node.y][mx].h;
+        out[count].g = map.map[node.y][mx].g;
+        out[count].y = node.y;
+        out[count++].x = mx;
+    }
+    if (!(map.height < py) && map.map[py][node.x].walkable == true)
+    {
+        out[count].parent = NULL;
+        out[count].f = map.map[py][node.x].f;
+        out[count].h = map.map[py][node.x].h;
+        out[count].g = map.map[py][node.x].g;
+        out[count].y = py;
+        out[count++].x = node.x;
+    }
+    return count;
+}
+
+Path backtrace(Node_s *endNode)
+{
+    int len = 1;
+    Point* endPath = (Point*)malloc(len * sizeof(Point));
+    Node_s* current = endNode;
+    endPath[len - 1].X = current->x;
+    endPath[len - 1].Y = current->y;
+    while (current->parent != NULL)
+    {
+        len++;
+        current = current->parent;
+        endPath = (Point *)realloc(endPath, sizeof(Point) * len);
+        endPath[len - 1].X = current->x;
+        endPath[len - 1].Y = current->y;
+    }
+
+    Point* path = (Point *)malloc(len * sizeof(Point));
+    for (int i = 0; i < len; i++)
+    {
+        path[i].X = endPath[len - 1 - i].X;
+        path[i].Y = endPath[len - 1 - i].Y;
+    }
+
+    // it gets returned in the reverse order
+    Path out = {
+        .length = len,
+        .path = path
+    };
+
+    return out;
+}
+
+/**
+ * @brief find path between two points
+ * 
+ * @param map map of the maze
+ * 
+ * @param start starting x and y coordinates
+ * 
+ * @param end ending x and y coordinates
+ * 
+ * @returns Path struct with all points that have to be visited to get from start to end
+*/
+Path path_find(Map map, Point start, Point end)
+{
+    Node_heap_s heap;
+
+    heap.first = NULL;
+    heap.last = NULL;
+    Node_s *startNode = (Node_s *)malloc(sizeof(Node_s));
+    Node_s *node;
+    Node_s endNode;
+    int x, y, i, l;
+    double ng;
+    endNode.x = end.X;
+    endNode.y = end.Y;
+    startNode->x = start.X;
+    startNode->y = start.Y;
+
+    Path out = {
+        .length = -1,
+        .path = NULL
+    };
+
+    startNode->f = 0.0f;
+    map.map[startNode->y][startNode->x].g = 0.0f;
+
+    startNode->parent = NULL;
+
+    push(&heap, startNode);
+
+    while (!is_empty(&heap))
+    {
+        node = pop(&heap);
+        if (node == NULL)
+        {
+            free_heap(&heap);
+
+            return out;
+        }
+
+        map.map[node->y][node->x].closed = true;
+
+        if (equal_nodes(*node, endNode))
+        {
+            Path final_path = backtrace(node);
+
+            free_heap(&heap);
+
+            return final_path;
+        }
+
+        Node_s neighbours[8];
+        int numNodes = get_neighbours(map, *node, neighbours);
+
+        for (i = 0; i < numNodes; i++)
+        {
+            Node_s *neighbour = (Node_s *)malloc(sizeof(Node_s));
+            neighbour->x = neighbours[i].x;
+            neighbour->y = neighbours[i].y;
+            neighbour->f = neighbours[i].f;
+            neighbour->g = neighbours[i].g;
+            neighbour->h = neighbours[i].h;
+            neighbour->parent = neighbours[i].parent;
+
+            x = neighbour->x;
+            y = neighbour->y;
+
+            if (map.map[neighbour->y][neighbour->x].closed == true)
+            {
+                free(neighbour);
+                continue;
+            }
+
+            ng = neighbour->g + ((x - node->x == 0 || y - node->y == 0) ? 1 : SQRT2);
+
+            // printf("B: %d\n", (!map[y][x].opened || ng < neighbour->g));
+            if ((!map.map[y][x].opened) || ng < neighbour->g)
+            {
+                neighbour->g = ng;
+                neighbour->h = neighbour->h == -1 ? WEIGHT * MANHATTAN(abs(x - endNode.x), abs(y - endNode.y)) : neighbour->h;
+                neighbour->f = neighbour->g + neighbour->h;
+                neighbour->parent = node;
+
+                map.map[y][x].f = neighbour->f;
+                map.map[y][x].g = neighbour->g;
+                map.map[y][x].h = neighbour->h;
+
+                if (!map.map[y][x].opened)
+                {
+                    // printf("P\n");
+                    map.map[y][x].opened = true;
+                    push(&heap, neighbour);
+                    continue;
+                }
+                else
+                {
+                    // printf("U\n");
+                    update(&heap, neighbour);
+                    continue;
+                }
+                free(neighbour);
+            }
+        }
+    }
+
+    free_heap(&heap);
+
+    return out;
+}
+
+#endif
